e326fc97492e9528fec9d9294b8bf27c98373866
David <david.nikolovski.00@gmail.com>
2023-10-24 12:41:57 +0200

    added signature field

--- src/components/Form/FormDivs/Form11.tsx
+++ src/components/Form/FormDivs/Form11.tsx
@@ -1,9 +1,13 @@
-import { Form, InputGroup } from "react-bootstrap";
+import { Button, Form, InputGroup, Modal } from "react-bootstrap";
 import PreviousBtn from "../../Buttons/PreviousBtn";
 import PrimaryButton from "../../Buttons/PrimaryButton";
 import { IEnteredData } from "../FormComponent";
 import { H3FormHeading } from "./Form1";
 import PrivacyLogo from "../../../assets/images/privacy.png";
+import { useCallback, useRef, useState } from "react";
+import styled from "styled-components";
+import SignatureCanvas from "react-signature-canvas";
+import { theme } from "../../../styles/themeStyles";
 
 interface Props {
   setFormControl: any;
@@ -20,14 +24,28 @@ const Form11 = ({
   percentageAddHandler,
   setEnteredData,
 }: Props) => {
+  const [showModal, setShowModal] = useState(false);
+
+  // const [canvas, setCanvas] = useState(undefined);
+
+  const padRef = useRef<SignatureCanvas | null>(null);
+
   const clickPrevHandler = () => {
     setFormControl((prev: number) => prev - 1);
     percentageRemoveHandler();
   };
   const clickNextHandler = () => {
-    setFormControl((prev: number) => prev + 1);
+    setShowModal(true);
     percentageAddHandler(5);
   };
+
+  // Modal handlers
+
+  const clearSignatureCanvas = useCallback(() => {
+    padRef?.current?.clear();
+
+    // setCanvas(undefined);
+  }, []);
 
   return (
     <div id="form-9">
@@ -80,8 +98,57 @@ const Form11 = ({
         stateSetter={clickNextHandler}
       />
       <PreviousBtn clickHandler={clickPrevHandler} />
+      <Modal
+        size="lg"
+        show={showModal}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            <H3FormHeading>
+              Great news, you are eligible to make a claim! Sign here to start
+              your claim online NOW.
+            </H3FormHeading>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SignatureDiv>
+            <SignatureCanvas ref={padRef} />
+          </SignatureDiv>
+
+          <ButtonsDiv>
+            {" "}
+            <Button
+              variant="danger"
+              size="lg"
+              onClick={() => clearSignatureCanvas()}
+            >
+              CLEAR
+            </Button>
+            <Button
+              variant="success"
+              size="lg"
+              onClick={() => setShowModal(false)}
+            >
+              SUBMIT
+            </Button>
+          </ButtonsDiv>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
 
 export default Form11;
+const ButtonsDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const SignatureDiv = styled.div`
+  border: 2px solid ${theme.colors.greys.second};
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
+  margin-bottom: 30px;
+`;
--- src/sections/FormSection.tsx
+++ src/sections/FormSection.tsx
@@ -4,49 +4,34 @@ import SslIcon from "../assets/images/SslIcon";
 
 import ProgressBar from "../components/ProgressBar";
 import FormComponent from "../components/Form/FormComponent";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const FormSection = () => {
   const [percentProgressBarArr, setPercentProgressBarArr] = useState<number[]>(
     []
   );
-  const [arrayCounter, setArrayCounter] = useState<number>(0);
 
-  useEffect(() => {
-    const sumWithInitial = percentProgressBarArr.reduce(
-      (accumulator: number, currentValue: number) => accumulator + currentValue,
-      0
-    );
-    setArrayCounter(sumWithInitial);
-  }, [percentProgressBarArr]);
   return (
-    <>
-      {arrayCounter !== 100 ? (
-        <FormSectionStyled>
-          <Container>
-            <Row>
-              <Col className="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-12 text-center position-relative">
-                <ProgressBarDiv>
-                  <ProgressBar percentageArr={percentProgressBarArr} />
-                </ProgressBarDiv>
-                <FormComponent setterPercentage={setPercentProgressBarArr} />
-                <div style={{ paddingBottom: "20px" }}>
-                  <SslIcon />
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </FormSectionStyled>
-      ) : (
-        <div>Signature section</div>
-      )}
-    </>
+    <section>
+      <Container>
+        <Row>
+          <Col className="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-12 text-center position-relative">
+            <ProgressBarDiv>
+              <ProgressBar percentageArr={percentProgressBarArr} />
+            </ProgressBarDiv>
+            <FormComponent setterPercentage={setPercentProgressBarArr} />
+            <div style={{ paddingBottom: "20px" }}>
+              <SslIcon />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
 export default FormSection;
 
-const FormSectionStyled = styled.section``;
 const ProgressBarDiv = styled.div`
   width: 120px;
   height: 120px !important;
